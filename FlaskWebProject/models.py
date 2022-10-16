0bbd243aaca9da4ab6f58953bb4903c0f9840108
faisal3397 <faisal.alowaisi.97@gmail.com>
2022-10-17 00:44:09 +0300

    my OCD was triggered

--- FlaskWebProject/models.py
+++ FlaskWebProject/models.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-
 from FlaskWebProject import app, db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
